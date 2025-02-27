005fbe9cef7b7830118f95abbf990dbf12755046
Bruno <brunopokemaster0@gmail.com>
2025-02-27 09:39:13 -0300

    exibindo quantrdade de itens na lista

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const category = document.getElementById("category");
 
 // Seleciona os elementos da lista
 const expenseList = document.querySelector("ul");
+const expensesQuantity = document.querySelector("aside header p span")
 
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
@@ -50,6 +51,7 @@ form.onsubmit = () => {
     expenseAdd(newExpense)
 }
 
+// Adiciona um novo item na lista
 function expenseAdd(newExpense) {
     try {
         //Cria o elemento para adicionar na lista para adicionar o item (li) na lista (ul).
@@ -94,5 +96,23 @@ function expenseAdd(newExpense) {
     } catch(error) {
         alert("Não foi possivel atualizar a lista de despesas")
         console.log(error)
+    }
+
+    //Atualiza os totais
+    updateTotals()
+}
+
+// Atualiza os totais
+function updateTotals() {
+    try {
+        // Recupera todos os itens (li) da lista (ul)
+        const items = expenseList.children
+        
+        // Atualiza a quantidade de itens da lista
+        expensesQuantity.textContent = `${items.length} 
+        ${items.length > 1 ? "despesas" : "despesa"}`
+    } catch (error) {
+        console.log(error)
+        alert("Não foi possível atualizar os totais")
     }
 }
