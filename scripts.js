a0155597b74fe848dbfef2144a4a7a8d438e367e
Bruno <brunopokemaster0@gmail.com>
2025-02-27 09:20:41 -0300

    adicionando botão remover

--- scripts.js
+++ scripts.js
@@ -81,8 +81,13 @@ function expenseAdd(newExpense) {
         expenseAmount.classList.add("expense-amount");
         expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
 
+        // Cria o ícone de remover
+        const removeIcon = document.createElement("img");
+        removeIcon.classList.add("remove-icon");
+        removeIcon.setAttribute("src", "assets/remove.svg");
+        removeIcon.setAttribute("alt","remover");
         // Adiciona as informações no item
-        expenseItem.append(expenseIcon, expenseInfo, expenseAmount)
+        expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
         // Adiciona o item na lista
         expenseList.append(expenseItem);
